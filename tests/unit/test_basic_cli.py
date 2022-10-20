ae31823d9618f31ede3e8a114be9d46633e3bb2f
Brian Walsh <brian@bwalsh.com>
2022-10-20 15:59:30 -0700

    Adds initial tests

---
+++ tests/unit/test_basic_cli.py
@@ -0,0 +1,15 @@
+
+
+def test_the_version():
+    """The CLI should print the version."""
+    raise NotImplementedError("TODO - implement functionality")
+
+
+def test_identify_the_endpoint():
+    """The CLI should print the version."""
+    raise NotImplementedError("TODO - implement functionality")
+
+
+def test_check_credentials():
+    """The CLI should print the version."""
+    raise NotImplementedError("TODO - implement functionality")
