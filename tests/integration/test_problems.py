e4866ad4dd4605885b6934490ef7b95d933af7c0
Brian Walsh <brian@bwalsh.com>
2022-12-14 09:31:43 -0800

    Fix docstring

--- tests/integration/test_problems.py
+++ tests/integration/test_problems.py
@@ -2,15 +2,15 @@ from click.testing import CliRunner
 from drs_downloader.cli import cli
 
 
-def test_terra_bad():
-    """The terra command should execute without error."""
+def test_terra_bad_tsv():
+    """The terra command should execute with an error."""
     runner = CliRunner()
     result = runner.invoke(cli, ['terra', '--manifest_path', 'tests/fixtures/terra-data-bad.tsv'])
     assert result.exit_code != 0
 
 
-def test_gen3_problem():
-    """The gen3 command should execute without error."""
+def test_gen3_bad_tsv():
+    """The gen3 command should execute with an error."""
     runner = CliRunner()
     result = runner.invoke(cli, ['gen3', '--endpoint', 'https://development.aced-idp.org', '--manifest_path',
                                  'tests/fixtures/gen3-bad.tsv'])
